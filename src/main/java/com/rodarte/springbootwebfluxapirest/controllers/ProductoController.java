a4c6ab77824a178559e31533df3e0b4e31cce5d4
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2020-02-19 20:13:43 -0700

    59. Implementando handler GET ver detalle en RestController

--- src/main/java/com/rodarte/springbootwebfluxapirest/controllers/ProductoController.java
+++ src/main/java/com/rodarte/springbootwebfluxapirest/controllers/ProductoController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
@@ -31,6 +32,27 @@ public class ProductoController {
                 .contentType(MediaType.APPLICATION_JSON)
                 .body(this.productoService.findAll())
         );
+    }
+
+    @GetMapping("/{id}")
+    public Mono<ResponseEntity<Producto>> ver(@PathVariable String id) {
+
+        return this
+                .productoService
+                .findById(id)
+                .map(
+                    producto ->
+                        ResponseEntity
+                            .ok()
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .body(producto)
+                )
+                .defaultIfEmpty(
+                    ResponseEntity
+                        .notFound()
+                        .build()
+                );
+
     }
 
 }
